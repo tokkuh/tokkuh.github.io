de82e0cf16315fe3e74bd29374f064f330d1e789
tokkuh <94223066+tokkuh@users.noreply.github.com>
2021-12-17 16:10:12 +0900

    Add files via upload

---
+++ information.bat
@@ -0,0 +1,6 @@
+@echo off
+start "score-up" "\\10.11.36.16\共有フォルダ\joho\1-10\03-スコアアップシート\スコアアップシート -  (27).xlsx"
+start explorer.exe
+start "GoldFingerSchool" "C:\Program Files (x86)\bcom\Gold Finger School\GFStart.EXE"
+start microsoft-edge:http://www.hauru110.tk/dev
+rem call TeamsをEdgeで(プライベートモード).bat
